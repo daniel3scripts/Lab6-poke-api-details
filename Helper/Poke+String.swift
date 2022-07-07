0411bc5d3cd9686718d71dd9c9666806e3cfcabc
MAC15 <mac15@MacBook-Air-de-MAC15.local>
2022-07-07 18:54:54 -0500

    commit poke

---
+++ Helper/Poke+String.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+class HelperString {
+    
+    static func getIdFromUrl(url: String) -> String {
+        // js: url.split("/").filter(element => element != "").pop()
+        // swift: url.components(separatedBy: "/").filter({ $0 != ""}).last!
+        return url.components(separatedBy: "/").filter({ $0 != ""}).last!
+    }
+    
+    static func getURLFromString(url: String) -> URL? {
+        guard let url = URL(string: url) else { return nil }
+        
+        return url
+    }
+}
+
